a8f5ecc7dec1f6aa6306091ccdf77941f8c43fb4
Dhiya Ulhaq Ramadhan <162105398+Dhiyaulll@users.noreply.github.com>
2025-06-13 13:44:10 +0700

    Update AreaCalculator.java

--- src/main/java/AreaCalculator.java
+++ src/main/java/AreaCalculator.java
@@ -5,7 +5,7 @@ public class AreaCalculator {
 	public static double calculateArea(ShapeType shapeType, double... dimensions) {
 		switch (shapeType) {
 			case CIRCLE:
-				if (dimensions.length != satu) {
+				if (dimensions.length != 1) {
 					throw new IllegalArgumentException("This CIRCLE needs 1 dimension (radius)");
 				}
 				return Math.PI * dimensions[0] * dimensions[0];
